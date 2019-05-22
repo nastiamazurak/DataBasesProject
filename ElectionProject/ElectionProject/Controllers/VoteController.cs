53e152690a65a02e2cf45ab2022939e3e9a65540
Olexandr <olexandrdobra@hotmail.com>
2019-05-23 01:20:29 +0300

    cc

--- ElectionProject/ElectionProject/Controllers/VoteController.cs
+++ ElectionProject/ElectionProject/Controllers/VoteController.cs
@@ -3,24 +3,38 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ElectionProject.Data;
 using Microsoft.AspNetCore.Mvc;
 using ElectionProject.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ElectionProject.Controllers
 {
-    public class VoteController: Controller
+    public class VoteController : Controller
     {
+        private readonly ElectionContext _context;
+        private readonly IElectionService _electionService;
+
+        public VoteController(ElectionContext context, IElectionService electionService)
+        {
+            _context = context;
+            _electionService = electionService;
+        }
+
         public IActionResult Index()
         {
+            return View(_electionService.GetPermitedElections());
 
-            return View();
         }
 
 
-        public IActionResult Vote()
+        public async Task<IActionResult> Vote(int electionId)
         {
 
-            return View();
+           
+            var vote = (await _context.Candidate.Where(x => x.ElectionId == electionId).ToListAsync()).OrderBy(x => x.Id);
+            
+            return View(vote);
         }
 
 
